65dd85c6d33a7f0a0d5bd81267f4774ecf3782c5
marcoxmediran <marcoxmediran@gmail.com>
2024-02-28 10:57:54 +0800

    add harpoon binds

--- config/nvim/lua/config/binds.lua
+++ config/nvim/lua/config/binds.lua
@@ -7,3 +7,10 @@ vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { noremap = true, silent = true })
+
+-- Harpoon
+local harpoon = require("harpoon")
+vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<C-K>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-J>", function() harpoon:list():next() end)
--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -4,4 +4,7 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
+    config = function ()
+        require("harpoon"):setup()
+    end
 }

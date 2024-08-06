534fecea08248557bcacabbde1cfafcb9981bcc3
marcoxmediran <marcoxmediran@gmail.com>
2024-08-06 11:29:24 +0800

    revert to gruvbox material

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "sainnhe/gruvbox-material",
+    config = function()
+      vim.opt.termguicolors = true
+      vim.g.gruvbox_material_better_performance = 1
+      vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_enable_bold = true
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_transparent_background = 2
+      vim.cmd "colorscheme gruvbox-material"
+    end,
+  },
+}
--- config/nvim/lua/plugins/lackluster.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "slugbyte/lackluster.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.opt.background = "dark"
-    vim.cmd.colorscheme("lackluster-hack")
-  end,
-}

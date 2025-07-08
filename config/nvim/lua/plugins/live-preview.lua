cc31cf1f049fa18ca932a21b94c90d5de21f8c8b
marcoxmediran <marcoxmediran@gmail.com>
2025-07-08 10:41:16 +0800

    chore: update live-preview config

--- config/nvim/lua/plugins/live-preview.lua
+++ config/nvim/lua/plugins/live-preview.lua
@@ -1,6 +1,14 @@
 return {
-    'brianhuster/live-preview.nvim',
-    dependencies = {
-        'nvim-telescope/telescope.nvim',
-    },
+  "brianhuster/live-preview.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+  },
+  config = function()
+    local live_preview = require("live-preview")
+    live_preview.setup({
+      port = 5500,
+      sync_scroll = true,
+      picker = "telescope",
+    })
+  end,
 }

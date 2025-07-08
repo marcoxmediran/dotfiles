87fcfeaa826b3bd72998013aed57aec1c926f551
marcoxmediran <marcoxmediran@gmail.com>
2025-07-08 10:33:43 +0800

    feat: add telescope integration to harpoon

--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -5,15 +5,36 @@ return {
     "nvim-lua/plenary.nvim",
   },
   config = function()
-    require("harpoon"):setup()
-    -- Binds
     local harpoon = require("harpoon")
+    harpoon:setup()
+
+    -- basic telescope configuration
+    local conf = require("telescope.config").values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require("telescope.pickers")
+          .new({}, {
+            prompt_title = "Harpoon",
+            finder = require("telescope.finders").new_table({
+              results = file_paths,
+            }),
+            previewer = conf.file_previewer({}),
+            sorter = conf.generic_sorter({}),
+          })
+          :find()
+    end
+
+    -- Binds
     vim.keymap.set("n", "<leader>a", function()
       harpoon:list():add()
     end)
     vim.keymap.set("n", "<leader>e", function()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
+      toggle_telescope(harpoon:list())
+    end, { desc = "Open harpoon window" })
     vim.keymap.set("n", "<C-K>", function()
       harpoon:list():prev()
     end)

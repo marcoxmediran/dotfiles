799c675f12224457a2112a60bf226d8edf02f194
marcoxmediran <marcoxmediran@gmail.com>
2025-07-08 10:22:04 +0800

    refactor: remove dressing and indent-blankline for snacks

---
+++ config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    bigfile = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+  },
+}

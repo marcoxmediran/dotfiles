b8d6f8f5e129fde67317489c2b042bd7a33c568e
marcoxmediran <marcoxmediran@gmail.com>
2025-07-21 20:35:18 +0800

    feat: add laravel.nvim plugin

---
+++ config/nvim/lua/plugins/laravel.lua
@@ -0,0 +1,20 @@
+return {
+  "adalessa/laravel.nvim",
+  dependencies = {
+    "tpope/vim-dotenv",
+    "nvim-telescope/telescope.nvim",
+    "MunifTanjim/nui.nvim",
+    "kevinhwang91/promise-async",
+  },
+  cmd = { "Laravel" },
+  keys = {
+    { "<leader>la", ":Laravel artisan<cr>" },
+    { "<leader>lr", ":Laravel routes<cr>" },
+    { "<leader>lm", ":Laravel related<cr>" },
+  },
+  event = { "VeryLazy" },
+  opts = {
+    lsp_server = "intelephense",
+  },
+  config = true,
+}

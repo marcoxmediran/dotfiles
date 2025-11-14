ee4a06d44535d4b5da1a01ebf51cd7f2af93acfa
marcoxmediran <marcoxmediran@gmail.com>
2025-11-14 10:53:56 +0800

    feat: change script as toggle for conservation mode; move script to separate directory

---
+++ scripts/conservation_mode/toggle_conservation_mode.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+CONSERVATION_MODE_STATUS=$(cat /sys/bus/platform/drivers/ideapad_acpi/VPC2004\:00/conservation_mode)
+
+if [ $CONSERVATION_MODE_STATUS == "1" ]; then
+  # Turn off conservation mode
+  echo 0 | doas tee /sys/bus/platform/drivers/ideapad_acpi/VPC2004\:00/conservation_mode > /dev/null
+  # Notify if successful
+  if [ $? == 0 ]; then
+    notify-send "Conservation Mode turned off."
+  fi
+else
+  # Turn on conservation mode
+  echo 1 | doas tee /sys/bus/platform/drivers/ideapad_acpi/VPC2004\:00/conservation_mode > /dev/null
+  # Notify if successful
+  if [ $? == 0 ]; then
+    notify-send "Conservation Mode turned off."
+  fi
+fi

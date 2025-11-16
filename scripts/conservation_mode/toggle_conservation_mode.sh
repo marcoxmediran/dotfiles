b01ff274e224c5e3fc61937704e828480ab02c80
marcoxmediran <marcoxmediran@gmail.com>
2025-11-16 20:22:27 +0800

    fix: set correct message when turning on conservation mode

--- scripts/conservation_mode/toggle_conservation_mode.sh
+++ scripts/conservation_mode/toggle_conservation_mode.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 CONSERVATION_MODE_STATUS=$(cat /sys/bus/platform/drivers/ideapad_acpi/VPC2004\:00/conservation_mode)
+echo $CONSERVATION_MODE_STATUS
 
 if [ "$CONSERVATION_MODE_STATUS" == "1" ]; then
   # Turn off conservation mode
@@ -14,6 +15,6 @@ else
   echo 1 | doas tee /sys/bus/platform/drivers/ideapad_acpi/VPC2004\:00/conservation_mode > /dev/null
   # Notify if successful
   if [ $? == 0 ]; then
-    notify-send "Conservation Mode turned off."
+    notify-send "Conservation Mode turned on."
   fi
 fi

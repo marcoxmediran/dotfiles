58b64d5cd38d6242cf0b3815a1853f218d765e52
marcoxmediran <marcoxmediran@gmail.com>
2025-11-16 20:23:36 +0800

    feat: detect highest refresh rate for HDMI-A-0 output

--- scripts/xrandr/duplicate_screens.sh
+++ scripts/xrandr/duplicate_screens.sh
@@ -3,5 +3,14 @@
 HAS_MONITOR=`xrandr --listmonitors | grep HDMI-A-0`
 
 if [ $? == 0 ]; then
-  xrandr --output HDMI-A-0 --mode 1920x1080 --rate 100 --pos 0x0 --rotate normal --output eDP --auto --same-as HDMI-A-0
+  # Get highest monitor refresh rate
+  rate=$(xrandr \
+    | sed -n '/^HDMI-A-0 connected/,/connected/p' \
+    | grep -E '^\s+1920x1080' \
+    | grep -oE '[0-9]+\.[0-9]+' \
+    | sort -nr \
+    | head -1
+  )
+
+  xrandr --output HDMI-A-0 --mode 1920x1080 --rate $rate --pos 0x0 --rotate normal --output eDP --auto --same-as HDMI-A-0
 fi
--- scripts/xrandr/monitor_only.sh
+++ scripts/xrandr/monitor_only.sh
@@ -3,5 +3,14 @@
 HAS_MONITOR=`xrandr --listmonitors | grep HDMI-A-0`
 
 if [ $? == 0 ]; then
-  xrandr --output eDP --off --output HDMI-A-0 --primary --mode 1920x1080 --rate 100 --pos 0x0 --rotate normal
+  # Get highest monitor refresh rate
+  rate=$(xrandr \
+    | sed -n '/^HDMI-A-0 connected/,/connected/p' \
+    | grep -E '^\s+1920x1080' \
+    | grep -oE '[0-9]+\.[0-9]+' \
+    | sort -nr \
+    | head -1
+  )
+
+  xrandr --output eDP --off --output HDMI-A-0 --primary --mode 1920x1080 --rate $rate --pos 0x0 --rotate normal
 fi
